a0d2b689cd02f09fc20707ad574a548061924dc6
Azzam Enajjar <azzam@pdx.edu>
2015-07-19 11:23:48 -0700

    Add an activity for inserting a new date to the asthmaTime Table

--- app/src/main/java/edu/pdx/oss/asthmacontrol/MainActivity.java
+++ app/src/main/java/edu/pdx/oss/asthmacontrol/MainActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button NEW_LOG_BUTTON, UPDATE_LOG_BUTTON, REPORT_SCORE_BUTTON, EXIT_BUTTON;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NEW_LOG_BUTTON = (Button) findViewById(R.id.newLogButton);
+
+        NEW_LOG_BUTTON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewAsthmaTimeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
