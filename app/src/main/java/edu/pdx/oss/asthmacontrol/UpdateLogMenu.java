18ba6124d3d64670ae09a35d99776a2f3758ad9c
Azzam Enajjar <azzam@pdx.edu>
2015-07-21 17:28:58 -0700

    Add a sub-menu activity to select which activity by which date(s) will be updated

---
+++ app/src/main/java/edu/pdx/oss/asthmacontrol/UpdateLogMenu.java
@@ -0,0 +1,13 @@
+package edu.pdx.oss.asthmacontrol;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UpdateLogMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_log_menu);
+    }
+}
