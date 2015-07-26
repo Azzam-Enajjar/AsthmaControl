65ba092c2cf539f7e9c1332b337f44d65cda5470
Azzam Enajjar <azzam@pdx.edu>
2015-07-26 11:21:31 -0700

    Add a method to the DatabaseOperations class to return date(s) from the asthmaMedication table that fall within the past four weeks

--- app/src/main/java/edu/pdx/oss/asthmacontrol/DatabaseOperations.java
+++ app/src/main/java/edu/pdx/oss/asthmacontrol/DatabaseOperations.java
@@ -208,6 +208,21 @@ public class DatabaseOperations extends SQLiteOpenHelper {
         return CR;
     }
 
+    public Cursor getPastFourWeeksFromAsthmaMedication(DatabaseOperations dop){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -28);
+        SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd" );
+        String startDate = sdf.format(cal.getTime());
+        String endDate = sdf.format(new Date());
+
+        SQLiteDatabase SQ = dop.getReadableDatabase();
+        String columns[] = {TableData.TableInfo.ASTHMA_MEDICATION_DATE};
+        String selection = TableData.TableInfo.ASTHMA_MEDICATION_DATE + " BETWEEN ? AND ?";
+        String args[] = {startDate, endDate};
+        Cursor CR = SQ.query(TableData.TableInfo.ASTHMA_MEDICATION_TABLE, columns, selection, args, null, null, null);
+        return CR;
+    }
+
     public void deleteAllFromAsthmaTime(DatabaseOperations dop){
         SQLiteDatabase SQ = dop.getWritableDatabase();
         SQ.delete(TableData.TableInfo.ASTHMA_TIME_TABLE, null, null);
