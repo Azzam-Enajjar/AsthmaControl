2246af19c489403800c473b58420ee33f9e84ad3
Azzam Enajjar <azzam@pdx.edu>
2015-07-25 17:58:02 -0700

    Update the user interface of the AsthmaSymptoms Activity

--- app/src/main/java/edu/pdx/oss/asthmacontrol/UpdateLogMenu.java
+++ app/src/main/java/edu/pdx/oss/asthmacontrol/UpdateLogMenu.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class UpdateLogMenu extends AppCompatActivity {
-    Button UPDATEASTHMATIME_BUTTON, UPDATEASTHMABREATH_BUTTON, BACK_BUTTON;
+    Button UPDATEASTHMATIME_BUTTON, UPDATEASTHMABREATH_BUTTON, UPDATEASTHMASYMPTOMS_BUTTON, BACK_BUTTON;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,7 @@ public class UpdateLogMenu extends AppCompatActivity {
 
         UPDATEASTHMATIME_BUTTON = (Button) findViewById(R.id.updateAsthmaTime);
         UPDATEASTHMABREATH_BUTTON = (Button) findViewById(R.id.updateAsthmaBreath);
+        UPDATEASTHMASYMPTOMS_BUTTON = (Button) findViewById(R.id.updateAsthmaSymptoms);
         BACK_BUTTON = (Button) findViewById(R.id.backButton);
 
         UPDATEASTHMATIME_BUTTON.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +32,14 @@ public class UpdateLogMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UpdateLogMenu.this, UpdateAsthmaBreathActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        UPDATEASTHMASYMPTOMS_BUTTON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UpdateLogMenu.this, UpdateAsthmaSymptomsActivity.class);
                 startActivity(intent);
             }
         });
