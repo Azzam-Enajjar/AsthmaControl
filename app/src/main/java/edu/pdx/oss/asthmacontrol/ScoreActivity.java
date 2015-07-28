127d269ff175a9292b94ff2350a0b2057516c46d
Azzam Enajjar <azzam@pdx.edu>
2015-07-27 17:38:58 -0700

    Calculate the total score

--- app/src/main/java/edu/pdx/oss/asthmacontrol/ScoreActivity.java
+++ app/src/main/java/edu/pdx/oss/asthmacontrol/ScoreActivity.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
-    EditText DAYS_ASTHMA_TIME_TEXT, SCORE_ASTHMA_TIME_TEXT, DAYS_ASTHMA_BREATH_TEXT, SCORE_ASTHMA_BREATH_TEXT, DAYS_ASTHMA_SYMPTOMS_TEXT, SCORE_ASTHMA_SYMPTOMS_TEXT,  DAYS_ASTHMA_MEDICATION_TEXT, SCORE_ASTHMA_MEDICATION_TEXT;
+    EditText DAYS_ASTHMA_TIME_TEXT, SCORE_ASTHMA_TIME_TEXT, DAYS_ASTHMA_BREATH_TEXT, SCORE_ASTHMA_BREATH_TEXT, DAYS_ASTHMA_SYMPTOMS_TEXT, SCORE_ASTHMA_SYMPTOMS_TEXT,  DAYS_ASTHMA_MEDICATION_TEXT, SCORE_ASTHMA_MEDICATION_TEXT, RATE_ASTHMA_TEXT, TOTAL_SCORE_TEXT;
     Context ctx = this;
     Integer numberOfDays;
-    Integer score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +25,55 @@ public class ScoreActivity extends AppCompatActivity {
         SCORE_ASTHMA_SYMPTOMS_TEXT = (EditText) findViewById(R.id.scoreText3);
         DAYS_ASTHMA_MEDICATION_TEXT = (EditText) findViewById(R.id.daysText4);
         SCORE_ASTHMA_MEDICATION_TEXT = (EditText) findViewById(R.id.scoreText4);
+        RATE_ASTHMA_TEXT = (EditText) findViewById(R.id.scoreText5);
+        TOTAL_SCORE_TEXT = (EditText) findViewById(R.id.totalScoreText);
 
         numberOfDays = dop.getNumberOfDaysFromAsthmaTime(dop);
         DAYS_ASTHMA_TIME_TEXT.setText(numberOfDays.toString());
-        score = getScoreFromAsthmaTime();
-        SCORE_ASTHMA_TIME_TEXT.setText(score.toString());
+        Integer score1 = getScoreFromAsthmaTime();
+        SCORE_ASTHMA_TIME_TEXT.setText(score1.toString());
 
         numberOfDays = dop.getNumberOfDaysFromAsthmaBreath(dop);
         DAYS_ASTHMA_BREATH_TEXT.setText(numberOfDays.toString());
-        score = getScoreFromAsthmaBreath();
-        SCORE_ASTHMA_BREATH_TEXT.setText(score.toString());
+        Integer score2 = getScoreFromAsthmaBreath();
+        SCORE_ASTHMA_BREATH_TEXT.setText(score2.toString());
 
         numberOfDays = dop.getNumberOfDaysFromAsthmaSymptoms(dop);
         DAYS_ASTHMA_SYMPTOMS_TEXT.setText(numberOfDays.toString());
-        score = getScoreFromAsthmaSymptoms();
-        SCORE_ASTHMA_SYMPTOMS_TEXT.setText(score.toString());
+        Integer score3 = getScoreFromAsthmaSymptoms();
+        SCORE_ASTHMA_SYMPTOMS_TEXT.setText(score3.toString());
 
         numberOfDays = dop.getNumberOfDaysFromAsthmaMedication(dop);
         DAYS_ASTHMA_MEDICATION_TEXT.setText(numberOfDays.toString());
-        score = getScoreFromAsthmaMedication();
-        SCORE_ASTHMA_MEDICATION_TEXT.setText(score.toString());
+        Integer score4 = getScoreFromAsthmaMedication();
+        SCORE_ASTHMA_MEDICATION_TEXT.setText(score4.toString());
+
+        Integer totalScore = score1 + score2 + score3 + score4;
+        TOTAL_SCORE_TEXT.setText(String.valueOf(totalScore));
+
+
 
     }
 
     public Integer getScoreFromAsthmaTime(){
-       if (numberOfDays == 28)
-           score = 1;
-       else if ((numberOfDays >=22) && (numberOfDays<=27))
-           score = 2;
-       else if ((numberOfDays >=8) && (numberOfDays<=21))
-           score = 3;
-       else if ((numberOfDays >=1) && (numberOfDays<=20))
-           score = 4;
-       else
-           score = 5;
+        Integer score;
+        if (numberOfDays == 28)
+            score = 1;
+        else if ((numberOfDays >=22) && (numberOfDays<=27))
+            score = 2;
+        else if ((numberOfDays >=8) && (numberOfDays<=21))
+            score = 3;
+        else if ((numberOfDays >=1) && (numberOfDays<=20))
+            score = 4;
+        else
+            score = 5;
 
-       return score;
+        return score;
     }
 
     public Integer getScoreFromAsthmaBreath(){
+        Integer score;
+
         if (numberOfDays == 28)
             score = 1;
         else if ((numberOfDays >=9) && (numberOfDays<=27))
@@ -77,6 +87,8 @@ public class ScoreActivity extends AppCompatActivity {
     }
 
     public Integer getScoreFromAsthmaSymptoms (){
+        Integer score;
+
         if ((numberOfDays >=16) && (numberOfDays<=28))
             score = 1;
         else if ((numberOfDays >=7) && (numberOfDays<=15))
@@ -92,6 +104,8 @@ public class ScoreActivity extends AppCompatActivity {
     }
 
     public Integer getScoreFromAsthmaMedication (){
+        Integer score;
+
         if (numberOfDays == 28)
             score = 1;
         else if ((numberOfDays >=7) && (numberOfDays<=14))
